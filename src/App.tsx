d2f5516d904074912a1adee133c453eca5007f74
Erez Amihud <erezamihud@gmail.com>
2022-08-31 16:04:34 +0300

    added mvvm pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
 import './App.css';
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import {observer} from 'mobx-react'
+import { makeAutoObservable, action } from "mobx"
+
+export class Timer {
+    secondsPassed = 0
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    increaseTimer() {
+        this.secondsPassed += 1
+    }
+
+    
+    reset = action(() => {
+        this.secondsPassed = 0;
+    })
+}
+
+const TimerView = observer(() => {
+    const [timer] = useState(() => new Timer()) // See the Timer definition above.
+
+    useEffect(() => {
+      const handle = setInterval(() => {
+          timer.increaseTimer()
+      }, 1000)
+      return () => {
+          clearInterval(handle)
+      }
+    }, [timer])
+
+    return (<div>
+      <p>
+        {timer.secondsPassed}s
+        </p>
+        <button className="button" onClick={timer.reset}>
+          Reset
+        </button>
+      </div>
+    )
+})
+
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-
-  function reset() {
-    setSeconds(0);
-  }
-
-  useEffect(() => {
-    let interval = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
-    return () => clearInterval(interval);
-  }, [seconds]);
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {seconds}s
-        </p>
-        <button className="button" onClick={reset}>
-          Reset
-        </button>
+        <TimerView />
       </header>
     </div>
   );
